709e69811b244b7048e22335fb2af4a045b8f4fd
unknown <bilalelbedoui93@gmail.com>
2019-04-10 12:51:19 +0200

    testing index-of

--- staff/bilal-elbedoui/array-functions/index-of/index-of.testing.js
+++ staff/bilal-elbedoui/array-functions/index-of/index-of.testing.js
@@ -2,18 +2,21 @@
 
 suite('Index-of', function(){
 
-    test('return the element position in an array', function(){
+    var beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+    var value= 'bison';
 
-        var beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
-        var value= 'bison';
+    test('return the element position in an array', function(){
         
         var expectedResult= 1;
         
-        var result;
-        result = indexOf(beasts, value);
+        var result=indexOf(beasts, value);
 
         expect(result,expectedResult);
         
 
-    })
-})
+    });
+
+    test('should break on undefined array', function(){
+
+    });
+});
