87dd4763945352674927489d532bc6849db16dab
unknown <bilalelbedoui93@gmail.com>
2019-05-01 11:48:54 +0200

    working on profile

---
+++ staff/groups/los-mosquitos/skyflix/src/components/Profile/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import literals from './literals'
+
+function Profile({ lang, onDelete, }) {
+
+
+    return <div>
+
+        <div>
+
+            <img src="" className="profile image"></img>
+
+        </div>
+
+        <div>
+            <form>
+
+                <p>
+                    <label>Fullname</label> : <input type="text" name="fullname" disabled/>
+                </p>
+                <p>
+                    <label>email</label> : <input type="text" name="email" disabled/>
+                </p>
+                <p>
+                    <label>If you want to change your password, introduce the new one</label> : <input type="password" name="changePassword" />
+                    <button>Change your password</button>
+                </p>
+
+            </form>
+        </div>
+
+        <div>
+            <form>
+                <p>
+                    <label>If you want to cancel your suscription</label> : <button>Cancel suscription</button>
+                </p>
+            </form>
+        </div>
+
+    </div>
+
+
+}
+
+
+export default Profile
+
+
